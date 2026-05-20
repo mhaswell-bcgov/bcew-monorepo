b1f6357b5cbbaf8b9528910f7075a61cbdb76d43
robrien-bcgov <richard.obrien@gov.bc.ca>
2026-05-20 16:10:55 -0700

    add consistent headers for the belleville terminal theme

--- themes/design-system-wordpress-child-theme-belleville-terminal/functions.php
+++ themes/design-system-wordpress-child-theme-belleville-terminal/functions.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * Theme functions and definitions
+ *
+ * @package Design_System_WordPress_Child_Theme_Belleville_Terminal
+ */
+
+/**
  * Load Composer autoloader and verify required class exists.
  * If the autoloader or the required class is missing, halt plugin execution.
  *
