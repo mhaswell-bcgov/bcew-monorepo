c9738079c618d8e0dba608a1518103e6ca146c1c
robrien-bcgov <richard.obrien@gov.bc.ca>
2026-05-20 16:08:11 -0700

    fix lint

--- themes/design-system-wordpress-child-theme-belleville-terminal/functions.php
+++ themes/design-system-wordpress-child-theme-belleville-terminal/functions.php
@@ -1,8 +1,9 @@
 <?php
-
 /**
  * Load Composer autoloader and verify required class exists.
  * If the autoloader or the required class is missing, halt plugin execution.
+ *
+ * @package design-system-wordpress-child-theme-belleville-terminal
  */
 $autoloader_path = __DIR__ . '/vendor/autoload.php';
 if ( file_exists( $autoloader_path ) ) {
