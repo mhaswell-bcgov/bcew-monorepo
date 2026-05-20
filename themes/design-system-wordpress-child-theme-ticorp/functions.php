ca2f525c5882089b6c90db89ad42690b11aa6157
robrien-bcgov <richard.obrien@gov.bc.ca>
2026-05-20 15:49:19 -0700

    dswp-873 also add autoloading to ticorp theme

--- themes/design-system-wordpress-child-theme-ticorp/functions.php
+++ themes/design-system-wordpress-child-theme-ticorp/functions.php
@@ -6,3 +6,12 @@
  */
 
 // This file is required for WordPress theme detection and PHPUnit testing.
+
+/**
+ * Load Composer autoloader and verify required class exists.
+ * If the autoloader or the required class is missing, halt plugin execution.
+ */
+$autoloader_path = __DIR__ . '/vendor/autoload.php';
+if ( file_exists( $autoloader_path ) ) {
+    require_once $autoloader_path;
+}
