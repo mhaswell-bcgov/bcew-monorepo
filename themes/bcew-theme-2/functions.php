07e0fd5c34b84ad4e293365fef2a34501e73ad33
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-05-27 14:38:31 -0700

    appearance tools disabled except for admins

--- themes/bcew-theme-2/functions.php
+++ themes/bcew-theme-2/functions.php
@@ -13,3 +13,26 @@ $autoloader_path = __DIR__ . '/vendor/autoload.php';
 if ( file_exists( $autoloader_path ) ) {
     require_once $autoloader_path;
 }
+
+/**
+ * Enable appearance tools for super admins only.
+ *
+ * @param WP_Theme_JSON_Data $theme_json Theme JSON data object.
+ *
+ * @return WP_Theme_JSON_Data
+ */
+function extended_web_enable_appearance_tools_for_super_admins( $theme_json ) {
+    if ( ! is_super_admin() ) {
+        return $theme_json;
+    }
+
+    return $theme_json->update_with(
+        array(
+            'version'  => 3,
+            'settings' => array(
+                'appearanceTools' => true,
+            ),
+        )
+    );
+}
+add_filter( 'wp_theme_json_data_theme', 'extended_web_enable_appearance_tools_for_super_admins', 20 );
