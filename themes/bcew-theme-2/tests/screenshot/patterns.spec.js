b582102a3adae39b82b43c83fdb91d7a7d328a6e
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-05-29 08:35:44 -0700

    Update patterns.spec.js

--- themes/bcew-theme-2/tests/screenshot/patterns.spec.js
+++ themes/bcew-theme-2/tests/screenshot/patterns.spec.js
@@ -1,8 +1,8 @@
-import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+import { test } from '@wordpress/e2e-test-utils-playwright';
 
-test.describe('pattern', () => {
-    test.beforeEach(async ({ admin }) => {
+test.describe( 'pattern', () => {
+    test.beforeEach( async ( { admin } ) => {
         // Create a new post before each test
         await admin.createNewPost();
-    });
-});
+    } );
+} );
