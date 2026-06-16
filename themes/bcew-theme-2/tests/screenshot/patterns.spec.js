ac26a5caf775537c6a7789ac6c1f35875640513d
robrien-bcgov <richard.obrien@gov.bc.ca>
2026-06-16 16:02:49 -0700

    dswp-954 dedup bcew-theme-2 patterns & stylebook specs <https://citz-gdx.atlassian.net/browse/DSWP-954>

--- themes/bcew-theme-2/tests/screenshot/patterns.spec.js
+++ themes/bcew-theme-2/tests/screenshot/patterns.spec.js
@@ -1,8 +1,3 @@
-import { test } from '@wordpress/e2e-test-utils-playwright';
+import { createPatternTests } from '@bcew-monorepo/e2e';
 
-test.describe( 'pattern', () => {
-    test.beforeEach( async ( { admin } ) => {
-        // Create a new post before each test
-        await admin.createNewPost();
-    } );
-} );
+createPatternTests( 'bcew-theme-2', [ 'sample' ] );
