551ec7f19b2bc428a0521b7ab515ad582b0ee5b1
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-06-23 16:16:36 -0700

    Update site-logo.php

--- themes/bcew-theme-2/patterns/site-logo.php
+++ themes/bcew-theme-2/patterns/site-logo.php
@@ -11,10 +11,9 @@
  * @package BcewTheme2
  */
 
-$bcew_logo_url = esc_url( get_template_directory_uri() . '/assets/images/bcid_h_rgb_pos.png' );
-$bcew_home_url = esc_url( home_url( '/' ) );
-$bcew_logo_alt = esc_attr__( 'Government of British Columbia', 'bc-extended-web-theme' );
+$bcew_logo_url = get_template_directory_uri() . '/assets/images/bcid_h_rgb_pos.png';
+$bcew_home_url = home_url( '/' );
 ?>
 <!-- wp:image {"width":"180px","height":"auto","sizeSlug":"full","linkDestination":"custom"} -->
-<figure class="wp-block-image size-full is-resized"><a href="<?php echo $bcew_home_url; ?>"><img src="<?php echo $bcew_logo_url; ?>" alt="<?php echo $bcew_logo_alt; ?>" style="width:180px;height:auto" /></a></figure>
+<figure class="wp-block-image size-full is-resized"><a href="<?php echo esc_url( $bcew_home_url ); ?>"><img src="<?php echo esc_url( $bcew_logo_url ); ?>" alt="<?php echo esc_attr__( 'Government of British Columbia', 'bc-extended-web-theme' ); ?>" style="width:180px;height:auto" /></a></figure>
 <!-- /wp:image -->
